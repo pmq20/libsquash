3306c714dc2a15f5973659692a8c6b307676efca
Minqi Pan <pmq2001@gmail.com>
2016-12-19 17:15:35 +0800

    remove useless

--- fixture.h
+++
@@ -1,9 +0,0 @@
-#ifndef FIXTURE_H_B716AAB0
-#define FIXTURE_H_B716AAB0
-
-#include <stdint.h>
-#include <stddef.h>
-
-extern const uint8_t libsquash_fixture[];
-
-#endif /* end of include guard: FIXTURE_H_B716AAB0 */
