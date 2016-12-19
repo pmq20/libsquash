a5f9cd22d5588b00b449a6fdb81379f9e77be6bf
Minqi Pan <pmq2001@gmail.com>
2016-12-19 17:48:25 +0800

    move to enclose-io

--- test/main.c
+++ test/main.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016 Minqi Pan
+ * Copyright (c) 2016 Libsquash contributors
  *
  * This file is part of libsquash, distributed under the MIT License
  * For full terms see the included LICENSE file
