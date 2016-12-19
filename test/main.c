18d5bedef87e44d5b19a7ad7ca8b94bfc5bdf3c3
Minqi Pan <pmq2001@gmail.com>
2016-12-19 17:44:29 +0800

    add test

---
+++ test/main.c
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2016 Minqi Pan
+ *
+ * This file is part of libsquash, distributed under the MIT License
+ * For full terms see the included LICENSE file
+ */
+
+#include "squash.h"
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+	fprintf(stderr, "\n");
+	fflush(stderr);
+	return 0;
+}
