7ed71ff98b1fe6e161696ef47412444ad0a2dc22
Minqi Pan <pmq2001@gmail.com>
2016-12-26 16:30:16 +0800

    add ifndef for windows.h

--- include/squash/windows.h
+++ include/squash/windows.h
@@ -18,19 +18,52 @@ typedef SSIZE_T ssize_t;
 typedef DWORD64 sqfs_off_t;
 typedef uint32_t uid_t;		/* [???] user IDs */
 
+#ifndef S_IFIFO
 #define	S_IFIFO		0010000		/* [XSI] named pipe (fifo) */
+#endif
+
+#ifndef S_IFBLK
 #define	S_IFBLK		0060000		/* [XSI] block special */
+#endif
+
+#ifndef S_IFLNK
 #define	S_IFLNK		0120000		/* [XSI] symbolic link */
+#endif
+
+#ifndef S_IFSOCK
 #define	S_IFSOCK	0140000		/* [XSI] socket */
+#endif
 
+#ifndef S_ISBLK
 #define	S_ISBLK(m)	(((m) & S_IFMT) == S_IFBLK)	/* block special */
-#define	S_ISCHR(m)	(((m) & S_IFMT) == S_IFCHR)	/* char special */
-#define	S_ISDIR(m)	(((m) & S_IFMT) == S_IFDIR)	/* directory */
-#define	S_ISFIFO(m)	(((m) & S_IFMT) == S_IFIFO)	/* fifo or socket */
-#define	S_ISREG(m)	(((m) & S_IFMT) == S_IFREG)	/* regular file */
-#define	S_ISLNK(m)	(((m) & S_IFMT) == S_IFLNK)	/* symbolic link */
-#define	S_ISSOCK(m)	(((m) & S_IFMT) == S_IFSOCK)	/* socket */
+#endif
 
+#ifndef S_ISCHR
+#define	S_ISCHR(m)	(((m) & S_IFMT) == S_IFCHR)	/* char special */
+#endif
+
+#ifndef S_ISDIR
+#define	S_ISDIR(m)	(((m) & S_IFMT) == S_IFDIR)	/* directory */
+#endif
+
+#ifndef S_ISFIFO
+#define	S_ISFIFO(m)	(((m) & S_IFMT) == S_IFIFO)	/* fifo or socket */
+#endif
+
+#ifndef S_ISREG
+#define	S_ISREG(m)	(((m) & S_IFMT) == S_IFREG)	/* regular file */
+#endif
+
+#ifndef S_ISLNK
+#define	S_ISLNK(m)	(((m) & S_IFMT) == S_IFLNK)	/* symbolic link */
+#endif
+
+#ifndef S_ISSOCK
+#define	S_ISSOCK(m)	(((m) & S_IFMT) == S_IFSOCK)	/* socket */
+#endif
+
+#ifndef makedev
 #define	makedev(x,y)	((dev_t)(((x) << 24) | (y)))
+#endif
 
 #endif /* end of include guard: WINDOWS_H_A80B5674 */
