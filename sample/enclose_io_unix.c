bc03860dc392e1ec65d2a9240415674c48cf022b
Minqi Pan <pmq2001@gmail.com>
2017-01-13 19:25:22 +0800

    define int enclose_io_cwd_inside

--- sample/enclose_io_unix.c
+++ sample/enclose_io_unix.c
@@ -7,7 +7,7 @@
  */
 
 #include "enclose_io_common.h"
-#ifndef _WIN32
+
 #define ENCLOSE_IO_GEN_EXPANDED_NAME(path)	sqfs_name enclose_io_expanded_name; \
 						memcpy(enclose_io_expanded_name, enclose_io_cwd, enclose_io_cwd_len); \
 						size_t memcpy_len = strlen(path); \
@@ -15,6 +15,11 @@
 						memcpy(&enclose_io_expanded_name[enclose_io_cwd_len], (path), memcpy_len); \
 						enclose_io_expanded_name[enclose_io_cwd_len + memcpy_len] = '\0'
 
+int enclose_io_cwd_inside;
+sqfs_name enclose_io_cwd;
+size_t enclose_io_cwd_len;
+
+#ifndef _WIN32
 int enclose_io_chdir(const char *path)
 {
 	int ret;
