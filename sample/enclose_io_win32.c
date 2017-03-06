aa03b32150bf32c51ee89bfa9721232d387defd9
Minqi Pan <pmq2001@gmail.com>
2017-03-06 16:18:36 +0800

    write IoStatusBlock->Information

--- sample/enclose_io_win32.c
+++ sample/enclose_io_win32.c
@@ -942,6 +942,8 @@ EncloseIOpNtQueryDirectoryFile(
                         ret->FileName[retlen] = 0;
                         ret->FileNameLength = sizeof(ret->FileName[0]) * retlen;
 			ret->FileAttributes = EncloseIODType2FileAttributes(mydirent->d_type);
+                        IoStatusBlock->Information = (void*)(&ret->FileName[retlen]) - (void*)FileInformation + 1;
+                        assert(IoStatusBlock->Information >= sizeof(FILE_DIRECTORY_INFORMATION));
 			return STATUS_SUCCESS;
 		}
 	} else {
